a7033e38591ada5c8317aac4c44b135e7f089d0f
omputle <omputle@student.wethinkcode.co.za>
2020-11-05 20:10:23 +0200

    added root

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import sys
 from roots.roots import check_roots
+from root.root import check_root
+from reduce_equation.reduce import Reduction
+
+def main_function(equation):
+    print(equation)
+    sides = equation.split("=")
+    if (len(sides) != 2):
+        print("Enter valid equation")
+        return
+    Reduction(equation)
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
-        print("No arguements!")
+        print("No equations!")
+    elif (len(sys.argv) > 2):
+        print("Enter one equation!")
+    else:
+        main_function(sys.argv[1])
