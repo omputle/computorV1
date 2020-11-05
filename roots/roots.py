5da9551923ddc1c1c0dd8e17e378a676820a5ed9
omputle <omputle@student.wethinkcode.co.za>
2020-11-05 14:04:12 +0200

    can calculate roots

---
+++ roots/roots.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+def check_roots(a,b,c):
+    discriminant = b*b - 4*a*c
+    if a == 0:
+        print("Not quadratic")
+    elif discriminant < 0:
+        print("No roots")
+    else:
+        dis_sqrt = sqrt(discriminant)
+        x1 = (b - dis_sqrt)/(2*a)
+        x2 = (b + dis_sqrt)/(2*a)
+        print("x1: ", x1)
+        print("x2: ", x2)
+
+check_roots(1, 2, 1)
