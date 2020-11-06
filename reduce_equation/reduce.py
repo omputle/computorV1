7659d37e2a5a12bcbf7dd1e0e9d7262909dd3f80
omputle <omputle@student.wethinkcode.co.za>
2020-11-06 15:55:36 +0200

    Beta reduction: works. Still need to do error handling

--- reduce_equation/reduce.py
+++ reduce_equation/reduce.py
@@ -1,3 +1,7 @@
+import re
+import json
+
+# "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"
 class Reduction:
     def __init__(self, equation):
         sides = equation.split("=")
@@ -6,11 +10,79 @@ class Reduction:
         print("LHS: ", self.lhs)
         print("RHS: ", self.rhs)
     
-    def find_deg(self):
-        pass
+    def find_max_deg(self):
+        left_deg = self.convert_to_int(self.deg_find(self.lhs))
+        right_deg = self.convert_to_int(self.deg_find(self.rhs))
+        if max(left_deg) > max(right_deg):
+            print('left: ', left_deg)
+            return (max(left_deg))
+        else:
+            return (max(right_deg))
 
-    def get_coefficients(self):
-        pass
+    def create_zeros(self, num):
+        zeros = []
+        for i in range(num + 1):
+            zeros.append(i - i)
+        print(zeros)
+    
+    def find_coefficients(self):
+        sides = {}
+        left_co = self.coeff_find(self.lhs)
+        right_co = self.coeff_find(self.rhs)
+        left_co = self.convert_coeffs(left_co)
+        right_co = self.convert_coeffs(right_co)
+        print('LHS coefficients', left_co)
+        print('RHS coefficients', right_co)
+        sides['left'] = left_co
+        sides['right'] = right_co
+        return sides
 
     def reduced_list(self):
-        pass
+        sides = self.find_coefficients()
+        # print(json.dumps(sides, indent=4))
+        left = sides.get('left')
+        right = sides.get('right')
+        dif = len(left) - len(right)
+        # print(dif)
+        if dif > 0:
+            for i in range(dif):
+                right.append(float(i - i))
+        elif dif < 0:
+            dif = dif * -1
+            for i in range(dif):
+                left.append(float(i - i))
+        print(left)
+        print(right)
+        red_list = []
+        for i in range(len(left)):
+            red_list.append(left[i] - right[i])
+        return red_list
+
+    def deg_find(self, text_to_search):
+        pattern = re.compile(r'[xX]\^(\d*)')
+        matches = pattern.findall(text_to_search)
+        return matches
+
+    # "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0"
+    def coeff_find(self, text_to_search):
+        pattern = re.compile(r'([-\s]*[\d+\.]*\d+)\s\*\s[xX]')
+        matches = pattern.findall(text_to_search)
+        return matches
+
+    def convert_to_int(self, array):
+        int_array = []
+        for item in array:
+            try:
+                int_array.append(int(item))
+            except ValueError:
+                print('Non numeric degree')
+                break
+        int_array.sort()
+        return list(set(int_array))
+
+    def convert_coeffs(self, coeffs):
+        coeffs_array = []
+        for item in coeffs:
+            num = re.sub(r'\s+', '', item)
+            coeffs_array.append(float(num))
+        return coeffs_array
