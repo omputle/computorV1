fd8f50c7e61b76e9407a435fc934425154c0e6c3
omputle <omputle@student.wethinkcode.co.za>
2020-11-07 00:01:29 +0200

    typo fixed

--- quadratic/quadratic.py
+++ quadratic/quadratic.py
@@ -7,14 +7,14 @@ def check_roots(array):
     c = array[0]
     discriminant = b*b - 4*a*c
     if discriminant < 0:
-        print("Discriminant is strictly negative, the two solutions are:")
+        print("Discriminant is strictly negative, the two complex solutions are:")
         dis_sqrt = cmath.sqrt(discriminant)
         x1 = (-b - dis_sqrt)/(2*a)
         x2 = (-b + dis_sqrt)/(2*a)
         print(round(x1.real, 2) + round(x1.imag, 2) * 1j)
         print(round(x2.real, 2) + round(x2.imag, 2) * 1j)
     elif discriminant == 0:
-        print("Discriminant is zero, the solutions is:")
+        print("Discriminant is zero, the solution is:")
         dis_sqrt = sqrt(discriminant)
         x1 = (-b - dis_sqrt)/(2*a)
         print(round(x1, 2))
