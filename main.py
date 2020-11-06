00dfde87e24821de77ccdcd59ea71dc4d3d12018
omputle <omputle@student.wethinkcode.co.za>
2020-11-06 15:56:23 +0200

    can print reduced form

--- main.py
+++ main.py
@@ -3,13 +3,30 @@ from roots.roots import check_roots
 from root.root import check_root
 from reduce_equation.reduce import Reduction
 
+def find_degree():
+    pass
+
+def reduced_form(array):
+    disp = 'Reduced form: '
+    count = 0
+    for item in array:
+        if count != 0:
+            if item < 0:
+                disp = disp + ' - '
+            else:
+                disp = disp + ' + '
+        disp = disp + str(abs(item)) + ' X^' + str(count)
+        count = count + 1
+    disp = disp + ' = 0'
+    print(disp)
+
 def main_function(equation):
-    print(equation)
     sides = equation.split("=")
     if (len(sides) != 2):
         print("Enter valid equation")
         return
-    Reduction(equation)
+    red_obj = Reduction(equation)
+    reduced_form(red_obj.reduced_list())
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
