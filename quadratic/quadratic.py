13b82d0f72798e846f9b3cd6f75390e9310c54bc
omputle <omputle@student.wethinkcode.co.za>
2020-11-06 23:44:59 +0200

    Prints solutions with 2 d.p

--- quadratic/quadratic.py
+++ quadratic/quadratic.py
@@ -1,23 +1,27 @@
 from math import sqrt
+import cmath
 
 def check_roots(array):
     a = array[2]
     b = array[1]
     c = array[0]
     discriminant = b*b - 4*a*c
-    if a == 0:
-        print("Not quadratic")
-    elif discriminant < 0:
-        print("No roots")
+    if discriminant < 0:
+        print("Discriminant is strictly negative, the two solutions are:")
+        dis_sqrt = cmath.sqrt(discriminant)
+        x1 = (-b - dis_sqrt)/(2*a)
+        x2 = (-b + dis_sqrt)/(2*a)
+        print(round(x1.real, 2) + round(x1.imag, 2) * 1j)
+        print(round(x2.real, 2) + round(x2.imag, 2) * 1j)
     elif discriminant == 0:
         print("Discriminant is zero, the solutions is:")
         dis_sqrt = sqrt(discriminant)
-        x1 = (b - dis_sqrt)/(2*a)
-        print(x1)
+        x1 = (-b - dis_sqrt)/(2*a)
+        print(round(x1, 2))
     else:
         print("Discriminant is strictly positive, the two solutions are:")
         dis_sqrt = sqrt(discriminant)
         x1 = (-b - dis_sqrt)/(2*a)
         x2 = (-b + dis_sqrt)/(2*a)
-        print(x1)
-        print(x2)
+        print(round(x1, 2))
+        print(round(x2, 2))
