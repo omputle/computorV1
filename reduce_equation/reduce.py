7148373d053dbbd022d6f8a3d9c488edc21e6261
omputle <omputle@student.wethinkcode.co.za>
2020-11-07 09:23:24 +0200

    added comments and can do fractions

--- reduce_equation/reduce.py
+++ reduce_equation/reduce.py
@@ -1,21 +1,21 @@
 import re
+from fractions import Fraction
 
 class Reduction:
+    """
+        Reduces the equation into its simplest form. It splits it into the LHS
+        and RHS. Then it subtracts the RHS from both sides of the equation,
+        leaving the RHS to be zero.
+    """
     def __init__(self, equation):
         sides = equation.split("=")
         self.lhs = sides[0].strip()
         self.rhs = sides[1].strip()
-    
-    def find_max_deg(self):
-        left_deg = self.convert_to_int(self.deg_find(self.lhs))
-        right_deg = self.convert_to_int(self.deg_find(self.rhs))
-        if max(left_deg) > max(right_deg):
-            print('left: ', left_deg)
-            return (max(left_deg))
-        else:
-            return (max(right_deg))
 
     def find_coefficients(self):
+        """
+            Takes LHS & RHS and finds their coefficients. Returns them in a dictionary.
+        """
         sides = {}
         left_co = self.coeff_find(self.lhs)
         right_co = self.coeff_find(self.rhs)
@@ -26,6 +26,10 @@ class Reduction:
         return sides
 
     def reduced_list(self):
+        """
+            Subtracts the RHS from the LHS to find the reduced form of the
+            equation.
+        """
         sides = self.find_coefficients()
         left = sides.get('left')
         right = sides.get('right')
@@ -43,16 +47,25 @@ class Reduction:
         return red_list
 
     def deg_find(self, text_to_search):
+        """
+            Uses Regex to find the exponents of X.
+        """
         pattern = re.compile(r'[xX]\^(\d*)')
         matches = pattern.findall(text_to_search)
         return matches
 
     def coeff_find(self, text_to_search):
-        pattern = re.compile(r'([-\s]*[\d+\.]*\d+)\s\*\s[xX]')
+        """
+            Uses Regex to find the coefficients of the polynomial.
+        """
+        pattern = re.compile(r'([-\s]*\d+[\.\/]*[\d+]*)\s\*\s[xX]')
         matches = pattern.findall(text_to_search)
         return matches
 
     def convert_to_int(self, array):
+        """
+            Converts list of number characters into list of integers.
+        """
         int_array = []
         for item in array:
             try:
@@ -64,8 +77,11 @@ class Reduction:
         return list(set(int_array))
 
     def convert_coeffs(self, coeffs):
+        """
+            Converts list of number character into list of floats.
+        """
         coeffs_array = []
         for item in coeffs:
             num = re.sub(r'\s+', '', item)
-            coeffs_array.append(float(num))
+            coeffs_array.append(float(Fraction(num)))
         return coeffs_array
